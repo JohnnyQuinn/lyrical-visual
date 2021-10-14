a18034930d7a7cac6d9cf95bd950bdeacb8a26ed
Johnny Quinn <jftq97@gmail.com>
2021-10-13 20:39:22 -0400

    Restructured app.js, deleted auth.js and moved code to app.js, and updated README

--- README.md
+++ README.md
@@ -47,21 +47,21 @@ Searches for a complete transcript of a song’s lyrics in order to fully take i
 
 ### Phase 2:
 
-Implement spotify API
-Implement spotify authorization 
-Install 3rd party spotify node modules 
-Connect user to spotify account 
-Be able to search and play song
-Push to live hosting 
+- Implement spotify API
+- Implement spotify authorization 
+- Install 3rd party spotify node modules 
+- Connect user to spotify account 
+- Be able to search and play song
+- Push to live hosting 
 
 ### Phase 3:
 
-Display lyrics real time as song plays 
-Polish frontend 
-Push to live hosting
-Technical Requirements
-Frontend will be built using a templating engine 
-Backend will be built using node 
+- Display lyrics real time as song plays 
+- Polish frontend 
+- Push to live hosting
+- Technical Requirements
+- Frontend will be built using a templating engine 
+- Backend will be built using node 
 
 
 
--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const { request } = require('express');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const env = require('dotenv').config()
+const SpotifyWebApi = require('spotify-web-api-node'); //3rd party Spotify npm package
 const path = require('path');
 const cors = require('cors')
 
-// retrieving objects from auth.js
-const auth = require('./auth')
-const spotifyApi = auth.spotifyApi
 
+// --- SERVER SET UP ---
 const app = express();
 
+// set PORT to process.env.PORT (for Heroku) or 3000
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("\n Server listening on port " + PORT + "\n")
+});
+
+// --- FRONTEND TEMPLATING SET UP ---
 app.engine('handlebars', exphbs({
     //set default path for views
     layoutsDir: path.join(__dirname, '/views/layouts '),
@@ -21,27 +29,62 @@ app.set('views',path.join(__dirname,'views'))
 app.use(express.static('public'))
 .use(cors());
 
-// --- ROUTES ---
+// --- BASIC ROUTES ---
 
 // HOMEPAGE
 app.get('/', (req, res) => {
+    if(spotifyApi === undefined){
+      console.log(`\n spotifyApi object is undefined \n`)
+    }
+    else {
+      console.log(`\nspotifyAPI: ${spotifyApi}\n`)
+    }
     res.render('home');
 });
 
 // LOGIN
 app.get('/login', (req, res) => {
-    authURL = auth.authorizeURL
-    res.redirect(authURL)
+    res.redirect(authorizeURL)
 })
 
-// CALLBACK
-// app.get('/callback', (req, res) => {
-//     const access_token = req.query.code || null;
-//     console.log('\nAccess token: ' + access_token + '\n ');
-//     res.render('home');
-//     auth.setAccessToken(access_token)
-// })
+// --- AUTHENTICATION ---
+const scopes = [
+  'ugc-image-upload',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+  'streaming',
+  'app-remote-control',
+  'user-read-email',
+  'user-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'user-read-recently-played',
+  'user-follow-read',
+  'user-follow-modify'
+]
 
+// VARIABLES FOR AUTH
+const redirectUri = 'https://lyrical-visual.herokuapp.com/callback'
+const clientId = String(process.env.CLIENT_ID)
+const clientSecret = String(process.env.CLIENT_SECRET)
+
+const spotifyApi = new SpotifyWebApi({
+  redirectUri: redirectUri,
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+console.log(`\n --- Authentication URL --- \n${authorizeURL}\n --- Authentication URL --- `);
+
+// CALLBACK 
 app.get('/callback', (req, res) => {
     const error = req.query.error;
     const code = req.query.code;
@@ -63,11 +106,11 @@ app.get('/callback', (req, res) => {
         spotifyApi.setAccessToken(access_token);
         spotifyApi.setRefreshToken(refresh_token);
   
-        console.log('access_token:', access_token);
-        console.log('refresh_token:', refresh_token);
+        console.log('\naccess_token:', access_token);
+        console.log('\nrefresh_token:', refresh_token);
   
         console.log(
-          `Sucessfully retreived access token. Expires in ${expires_in} s.`
+          `\nSucessfully retreived access token. Expires in ${expires_in} s.`
         );
         res.send('Success! You can now close the window.');
   
@@ -84,28 +127,30 @@ app.get('/callback', (req, res) => {
         console.error('Error getting Tokens:', error);
         res.send(`Error getting Tokens: ${error}`);
       });
-  });
+});
 
 // REFRESH
 app.get('/refresh', (req, res) => {
-    auth.refreshAccessToken();
-    res.render('home');
+  spotifyApi.refreshAccessToken().then(
+    function(data) {
+      console.log('The access token has been refreshed!');
+  
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function(err) {
+      console.log('Could not refresh access token', err);
+    }
+  );
 })
 
 app.get('/getme', (req, res) => {
-
     (async () => {
         const me = await spotifyApi.getMe();
         console.log(me);
+        res.render('home')
     })().catch(e => {
         console.error(e);
+        res.render('home')
     });
-    res.render('home')
 })
-
-// set PORT to process.env.PORT (for Heroku) or 3000
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-    console.log("\n Server listening on port " + PORT + "\n")
-});
